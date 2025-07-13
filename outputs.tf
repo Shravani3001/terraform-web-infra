f7ad08d000443736deb5273516e889fa439e298a
Shravani3001 <shravanik3001@gmail.com>
2025-07-14 05:15:58 +0530

    Updated outputs.tf
    Removed unnecessary Terraform outputs (subnet IDs, ASG, VPC ID, target group arn) to simplify final output

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,3 @@
-output "bastion_public_ip" {
-    value = aws_instance.bastion.public_ip
-}
-
-output "public_subnet_1_id" {
-    value = aws_subnet.public-1.id
-}
-
-output "public_subnet_2_id" {
-    value = aws_subnet.public-2.id
-}
-
-output "private_subnet_1_id" {
-    value = aws_subnet.private-1.id
-}
-
-output "private_subnet_2_id" {
-    value = aws_subnet.private-2.id
-}
-
-output "aws_vpc_id" {
-    value = aws_vpc.main.id
-}
-
 output "alb_dns_name" {
   value = aws_lb.app_alb.dns_name
-}
-
-output "asg_name" {
-  value = aws_autoscaling_group.app_asg.name
-}
-
-output "target_group_arn" {
-  value = aws_lb_target_group.app_tg.arn
 }

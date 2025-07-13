64546fcf274e72f48820b4aabf63de2cfd0d4c18
Shravani3001 <shravanik3001@gmail.com>
2025-07-14 05:17:54 +0530

    Cleanup: removed unused key_name and public_key_path variables

--- variables.tf
+++ variables.tf
@@ -5,11 +5,3 @@ variable "region" {
 variable "instance_type" {
     default = "t2.micro"
 }
-
-variable "key_name" {
-    default = "web-infra-key"
-}
-
-variable "public_key_path" {
-    default = "./web-infra-key.pub"
-}

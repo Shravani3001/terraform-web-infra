b1c814b090a0093884b9beffebb7be8adffb4b89
Shravani3001 <shravanik3001@gmail.com>
2025-07-14 05:12:16 +0530

    Updated main.tf
    Removed Bastion host, SSH access, and key pair configuration. Updated security groups for ALB-to-EC2 traffic only.

--- main.tf
+++ main.tf
@@ -23,38 +23,8 @@ data "aws_ami" "ubuntu" {
     }
 }
 
-resource "aws_key_pair" "web-infra-key" {
-    key_name = var.key_name
-    public_key = file(var.public_key_path)
-}
-
-resource "aws_security_group" "bastion_sg" {
-    vpc_id = aws_vpc.main.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
 resource "aws_security_group" "ec2_sg" {
     vpc_id = aws_vpc.main.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        security_groups = [aws_security_group.bastion_sg.id]
-    }
 
     ingress {
         from_port       = 80
@@ -166,19 +136,6 @@ resource "aws_route_table_association" "private_assoc_1" {
 resource "aws_route_table_association" "private_assoc_2" {
     subnet_id = aws_subnet.private-2.id
     route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_instance" "bastion" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = var.instance_type
-    key_name = aws_key_pair.web-infra-key.key_name
-    subnet_id = aws_subnet.public-1.id
-    vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-    associate_public_ip_address = true
-
-    tags = {
-        Name = "Bastion"
-    }
 }
 
 resource "aws_lb" "app_alb" {
